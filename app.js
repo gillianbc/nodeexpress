8363fb57ca9366008094864ab5237893a6a4275a
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2018-07-28 12:50:07 +0100

    Pug - single variable passed in

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
   // to be unix or windows slashes
   // res.sendFile(path.join(__dirname,'views','index.html'));  //This works too
   // res.sendFile(path.join(__dirname, 'views/index.html'));
-  res.render('index');
+  res.render('index',{mystring: 'This is my string'});
 });
 
 app.listen(3000, () => {
