6e89e27d5541884e157add7788a96eab7d85c63e
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2018-07-28 12:52:56 +0100

    Pug - showing passed in list items in a ul

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
   // to be unix or windows slashes
   // res.sendFile(path.join(__dirname,'views','index.html'));  //This works too
   // res.sendFile(path.join(__dirname, 'views/index.html'));
-  res.render('index',{mystring: 'This is my string'});
+  res.render('index',{mylist: ['a','b']});
 });
 
 app.listen(3000, () => {
